4c3283bcba0e1b21fb1b48f47bd6ffa947472cd9
Sucipto <masucip@gmail.com>
2022-02-22 13:38:46 +0700

    Update Routing

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgbAccordionConfig, NgbActiveModal, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './home/home.component';
@@ -9,7 +9,7 @@ import { HomeComponent } from './home/home.component';
   styleUrls: ['./profile.component.scss'],
   providers: [NgbAccordionConfig]
 })
-export class ProfileComponent implements AfterViewInit {
+export class ProfileComponent implements AfterViewInit, OnDestroy {
   constructor(
     public modal : NgbModal,
     private route : Router
@@ -18,9 +18,14 @@ export class ProfileComponent implements AfterViewInit {
 
   ngOnInit(): void {
   }
+  ngOnDestroy(): void {
+      if(this.modal.hasOpenModals()){
+        this.modal.dismissAll()
+      }
+  }
   ngAfterViewInit(): void {
     this.modal.open(HomeComponent, {windowClass: 'modal-animation', modalDialogClass : 'modal-mode-1', centered:true, ariaLabelledBy: 'modal-basic-title', size: 'lg'}).result.then((result) => {
-      
+      this.route.navigate(['/'])
     }, (reason) => {
       this.route.navigate(['/'])
     })
